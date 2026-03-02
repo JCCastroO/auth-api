5bcbf950baf8da0c6441db59b13db1580c40f809
joao.oliveira <joao.oliveira@opah.com.br>
2026-03-02 09:03:31 -0300

    test: add dapper dependency injection test

---
+++ tests/Auth.Api.View.Tests/Integrated/DependencyInjectionTests.cs
@@ -0,0 +1,31 @@
+﻿using Auth.Api.View.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+
+namespace Auth.Api.View.Tests.Integrated;
+
+public class DependencyInjectionTests
+{
+    private readonly IServiceCollection _service = new ServiceCollection();
+    private readonly IServiceProvider _provider;
+
+    public DependencyInjectionTests()
+    {
+        _service.ConfigureServices(default!);
+
+        _provider = _service.BuildServiceProvider();
+    }
+
+    [Fact]
+    public void ShouldConfigureDapperDependency()
+    {
+        // Arrange
+
+        // Act
+        var service = _provider.GetRequiredService<IDbConnection>();
+
+        // Assert
+        Assert.NotNull(service);
+    }
+}
